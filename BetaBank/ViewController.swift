de472ab2b52714aabc4e9573da38968e79d09e5f
Yandolav <yandolav@mail.ru>
2026-02-17 16:52:32 +0300

    Initial Commit

---
+++ BetaBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BetaBank
+//
+//  Created by user on 17.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
